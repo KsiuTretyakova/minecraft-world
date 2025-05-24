4da7977d74f1a865b06df4175de36acab557a5c9
KsiuTretyakova <hrytsenkoks@gmail.com>
2025-05-24 15:34:33 +0300

    Program where we can add and remove blocks

--- src/Components/Block.jsx
+++ src/Components/Block.jsx
@@ -1,10 +1,16 @@
-export default function Block(position, onClick) {
+export default function Block({ position, onClick, onRemove }) {
     return (
         <mesh
             position={position}
             onClick={(e) => {
                 e.stopPropagation();
                 onClick(position);
+            }}
+            onContextMenu={(e) => {
+                // e.preventDefault();
+                // setSelectedBlock(position);
+                e.stopPropagation();
+                onRemove(position);
             }}
             castShadow
             receiveShadow
